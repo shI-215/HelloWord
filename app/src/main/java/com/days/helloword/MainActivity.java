cfeea8059f143418b1630fb4bb0e7f753e5f9a96
shI-215 <317741673@qq.com>
2019-12-24 21:44:50 +0800

    第一次修改

--- app/src/main/java/com/days/helloword/MainActivity.java
+++ app/src/main/java/com/days/helloword/MainActivity.java
@@ -1,14 +1,39 @@
 package com.days.helloword;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        加载布局文件
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        tv_text = (TextView) findViewById(R.id.tv_text);
+        tv_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                显示启动
+//                Intent intent = new Intent();
+//                intent.setClass(MainActivity.this, FristActivity.class);
+//                startActivity(intent);
+//                隐式启动
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+            }
+        });
     }
 }
